e9610d6a420350b6caa8a4a59d89966937342c16
dadang17 <iskandardzulqornain38@gmail.com>
2022-12-01 19:42:41 +0700

    user route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth.route");
-// const userRoute = require("./routes/user.route");
+const userRoute = require("./routes/user.route");
 const artikelRoute = require("./routes/artikel.route");
 
 require("dotenv").config();
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
-// app.use(userRoute);
+app.use(userRoute);
 app.use(authRoute);
 app.use(artikelRoute);
 
